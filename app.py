ed1e476b729ef262106caab252ef2769d8912104
Quan Hua <quanhua92@gmail.com>
2022-05-07 01:26:31 +0700

    add sticky cookie

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 import socket
 import time
-from flask import Flask, request
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     wait = request.args.get("wait", default=-1, type=int)
-    if wait > 0:
-        ms = wait / 1000
-        time.sleep(ms)
+    sticky = request.args.get("sticky", default=-1, type=int)
+
     output = f"Hostname: {socket.gethostname()}<br/>"
     for k, v in request.headers:
         output += f"{k}: {v}<br/>"
-    return output
+    resp = make_response(output)
+
+    if wait > 0:
+        ms = wait / 1000
+        time.sleep(ms)
+    
+    if sticky > 0:
+        resp.set_cookie("sticky", "any_value", max_age=sticky)
+    
+    return resp
 
 if __name__ == "__main__":
     from waitress import serve
