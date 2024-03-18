71fac6553f8cbba40573e5363c155da57d9998d3
Lev Knoblock <levknoblock@gmail.com>
2024-03-17 20:47:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import random
 import string
 import os
 import ssl
+from threading import Thread
 from flask import Flask, request, make_response, session, render_template
 
 app = Flask(__name__)
@@ -17,7 +18,18 @@ def health():
 def home():
     return render_template('index.html')
 
-if __name__ == "__main__":
+def run_http():
+    app.run(host='0.0.0.0', port=8080)
+
+def run_https():
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     context.load_cert_chain('/etc/ssl/certs/tls.crt', '/etc/ssl/certs/tls.key')
-    app.run(host='0.0.0.0', port=8080, ssl_context=context)
+    app.run(host='0.0.0.0', port=8443, ssl_context=context)
+
+if __name__ == "__main__":
+    http_thread = Thread(target=run_http)
+    http_thread.start()
+    https_thread = Thread(target=run_https)
+    https_thread.start()
+    http_thread.join()
+    https_thread.join()
